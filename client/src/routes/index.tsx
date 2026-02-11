b93c0fcc429e92b3b163249a7d618d7f91f920cf
Antonio Ivanovski <antonio-ivanovski@users.noreply.github.com>
2026-02-11 11:57:47 +0100

    initial commit

---
+++ client/src/routes/index.tsx
@@ -0,0 +1,32 @@
+import { useMutation } from "@tanstack/react-query";
+import { createFileRoute } from "@tanstack/react-router";
+import { hcWithType } from "server/dist/client";
+
+export const Route = createFileRoute("/")({
+	component: Index,
+});
+
+const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000";
+
+const client = hcWithType(SERVER_URL);
+
+function Index() {
+	const mutation = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await client.hello.$get();
+				if (!res.ok) {
+					console.log("Error fetching data");
+					return;
+				}
+				return await res.json();
+			} catch (error) {
+				console.log(error);
+			}
+		},
+	});
+
+	return <div></div>;
+}
+
+export default Index;
